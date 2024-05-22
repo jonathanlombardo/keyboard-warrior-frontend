17dbfb0976737b35a81832f9ac20ce220306bd4e
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-22 15:51:42 +0200

    add login guard

---
+++ src/components/AppLogin.vue
@@ -0,0 +1,33 @@
+<script>
+// import MyComp from './components/MyComp.vue';
+// import {store} from './store/index.js'
+
+export default {
+  data() {
+    return {
+      // store,
+      // ...
+    };
+  },
+
+  props: {
+    // ...
+  },
+
+  methods: {
+    // ...
+  },
+
+  components: {},
+
+  created() {
+    // ...
+  },
+};
+</script>
+
+<template>
+  <h1 class="text-center"><font-awesome-icon icon="fa-solid fa-hippo" class="me-3" />LOGIN my little lion!</h1>
+</template>
+
+<style lang="scss" scoped></style>
--- src/router.js
+++ src/router.js
@@ -1,16 +1,39 @@
 import { createWebHistory, createRouter } from "vue-router";
-
+import { store } from "./store";
 import AppHome from "./components/AppHome.vue";
+import AppLogin from "./components/AppLogin.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     component: AppHome,
     name: "home",
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    component: AppLogin,
+    name: "login",
   },
 ];
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// redirect all routes where login is required if user is not logged
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // this route requires auth
+    if (!store.authToken) {
+      next({ name: "login" });
+    } else {
+      next();
+    }
+  } else {
+    next(); // does not require auth
+  }
 });
